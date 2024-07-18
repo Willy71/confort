f12be7f6663d7619011c67f7dba44ede0581204c
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-18 14:47:41 -0300

    Update 06_Creditos.py

--- pages/06_Creditos.py
+++ pages/06_Creditos.py
@@ -97,18 +97,16 @@ st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e
 with st.container():
     col51, col52, col53, col54 = st.columns([4,0.5,1,0.5])
     with col51:
-        texto("Website made with Streamlit framework", 5, 'white')   
+        texto("Website made with Streamlit framework", 2, 'white')   
     with col53:            
-        st.text(" ")
         photo_link('', "https://i.postimg.cc/cJhYJnqx/streamlit-logo.jpg", 'https://streamlit.io/', 120)
         
         
 with st.container():
     col55, col56, col57, col58 = st.columns([4,0.5,1,0.5])
     with col55:
-        texto("Programmed with Python for Guillermo Cerato", 5, 'white')
+        texto("Programmed with Python for Guillermo Cerato", 2, 'blue')
     with col57:
-        st.text(" ")
         photo_link('', "https://i.postimg.cc/9Q3yg2th/python.png", 'https://www.python.org', 120)
         
         
