99bec660ba351e0e1d887f0dcd3d608d63fa623a
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-20 18:31:19 -0300

    Update 03_Figma.py

--- pages/03_Figma.py
+++ pages/03_Figma.py
@@ -72,7 +72,7 @@ centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%2
 st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e1e615;" /> """, unsafe_allow_html=True)
 centrar_texto("Imagen para la Historia 3", 3, 'white')
 centrar_texto("Integración con el Sistema de Reservas", 3, 'white')
-centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20003.png?raw=true", 400)
+centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20003.png?raw=true", 500)
 
 st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e1e615;" /> """, unsafe_allow_html=True)
 centrar_texto("Imagen para la historia 6", 3, "white")
