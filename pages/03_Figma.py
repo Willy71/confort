fe39a56bb76185415b9e85426b7b14234b77e831
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-20 19:31:53 -0300

    Update 03_Figma.py

--- pages/03_Figma.py
+++ pages/03_Figma.py
@@ -75,6 +75,11 @@ centrar_texto("Integración con el Sistema de Reservas", 3, 'white')
 centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20003.png?raw=true", 500)
 
 st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e1e615;" /> """, unsafe_allow_html=True)
+centrar_texto("Imagen para la Historia 4", 3, 'white')
+centrar_texto("Sección de Opiniones de Experiencias", 3, 'white')
+centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20004.png?raw=true", 500)
+               
+st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e1e615;" /> """, unsafe_allow_html=True)
 centrar_texto("Imagen para la Historia 5", 3, 'white')
 centrar_texto("Notificaciones de Reservas de Experiencias", 3, 'white')
 centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20005.png?raw=true", 400)
