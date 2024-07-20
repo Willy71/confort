8179d990c36242b8b68ac0aa4fd0cb8e07135fa1
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-20 17:20:13 -0300

    Update 03_Figma.py

--- pages/03_Figma.py
+++ pages/03_Figma.py
@@ -65,6 +65,12 @@ centrar_texto("Personalización de Mapas Interactivos", 3, 'white')
 centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20001.png?raw=true", 400)
 
 st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e1e615;" /> """, unsafe_allow_html=True)
+centrar_texto("Imagen para la Historia 2", 3, 'white')
+centrar_texto("Filtro de Puntos de Interés", 3, 'white')
+centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20002.png?raw=true", 400)
+
+
+st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e1e615;" /> """, unsafe_allow_html=True)
 centrar_texto("Imagen para la historia 6", 3, "white")
 centrar_texto("Implementación de Mapas Interactivos", 3, "white")  
 centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/Historia%20006.png?raw=true", 400)
