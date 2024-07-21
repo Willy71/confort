fbe87cb9909c72fe334f6db47ad8fe76d7b719cb
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-21 11:37:34 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -63,5 +63,13 @@ def photo_link_center(alt_text, img_url, link_url, img_width):
 
 centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/titular3.png?raw=true", 600)
 
+with st.container():
+    col01, col02, col03 = st.columns(3)
+    with col01:
+        centrar_imagen("", 300)
+    with col03:
+        if st.button("Siguiente"):
+            st.switch_page("pages/01_Encuesta_para_relevar_info")
+
 
 
