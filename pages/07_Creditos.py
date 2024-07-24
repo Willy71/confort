f53a1d99c720480ddd51eb5cb3e45946841500f7
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-24 11:58:55 -0300

    Update 07_Creditos.py

--- pages/07_Creditos.py
+++ pages/07_Creditos.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
     page_title="Creditos",
-    page_icon="🌎",
+    page_icon="©",
     layout="wide"
 )
 
