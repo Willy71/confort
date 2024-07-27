f69d5499489c9755fd1de29bf15f9fab0d989f92
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-26 21:56:29 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 # Colocar nome na pagina, icone e ampliar a tela
 st.set_page_config(
@@ -8,24 +9,10 @@ st.set_page_config(
 )
 
 ###########################################################################################################
-# Track Google Analitics
-GA_TRACKING_ID = 'G-ZY2745B9DJ'
-
-ga_script = f"""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZY2745B9DJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZY2745B9DJ');
-</script>
-"""
-
-# Incrustar el código en la página
-st.components.v1.html(ga_script, height=0)
-
+# Include Google Analytics tracking code
+with open("google_analytics.html", "r") as f:
+    html_code = f.read()
+    components.html(html_code, height=0)
 ###########################################################################################################
 
 page_bg_img = f"""
