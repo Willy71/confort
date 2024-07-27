cafd54fd0cd6b4259312a29bc3cb917f7264a160
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-26 22:21:11 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,10 +9,22 @@ st.set_page_config(
 )
 
 ###########################################################################################################
+st.markdown(
+    """
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-**********"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-**********');
+        </script>
+    """, unsafe_allow_html=True)
+
 # Include Google Analytics tracking code
-with open("google_analytics.html", "r") as f:
-    html_code = f.read()
-    components.html(html_code, height=0)
+#with open("google_analytics.html", "r") as f:
+#    html_code = f.read()
+#    components.html(html_code, height=0)
 ###########################################################################################################
 
 page_bg_img = f"""
