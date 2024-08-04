f1c57013c2a79524fd19abd2ddcd33f85681e922
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-04 10:51:06 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -126,11 +126,12 @@ if img_base64:
         unsafe_allow_html=True,
     )
 
-st.write("#")
 centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/2.png?raw=true", 1200)
 #centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/titular3.png?raw=true", 450)
 #centrar_imagen("https://github.com/Willy71/confort/blob/main/pictures/001.png?raw=true", 300)
 
+st.write("#")
+st.header("")
 with st.container():
     col01, col02, col03 = st.columns(3)
     with col03:
