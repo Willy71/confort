e92d2c44e3dbb361ef5ccdbb678aa80d84e20e24
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-05 15:07:33 -0300

    Update 07_Creditos.py

--- pages/07_Creditos.py
+++ pages/07_Creditos.py
@@ -127,8 +127,8 @@ st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#e
 centrar_texto("Herramientas utilizadas.", 1, 'white' )
 
 with st.container():
-    col60, col61, col62, col63, col64 = st.columns([3,4,1,4,1.5])
-    with col61:
+    col60, col61, col62, col63, col64 = st.columns([2,5,1,5,0.5])
+    with col61
         left_texto_link("Google Docs", "https://docs.google.com/document/u/0/",  3, "white")
         left_texto_link("Jira", "https://www.atlassian.com/software/jira",  3, "white")
         left_texto_link("Figma", "https://www.figma.com/es-la/",  3, "white")
