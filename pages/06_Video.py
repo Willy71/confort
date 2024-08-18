80a1c445087b45b0592fdaedd7899aac98b2c25c
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-18 20:47:45 -0300

    Update 06_Video.py

--- pages/06_Video.py
+++ pages/06_Video.py
@@ -68,9 +68,9 @@ def video(url):
         embed_url = f"https://www.youtube.com/embed/{video_id}"
         st.markdown(f"""
                     <div style="display: flex; justify-content: center;">
-                        <iframe width="1280" height="720"
+                        <iframe width="1280" height="300"
                         src="{embed_url}" 
-                        frameborder="0">
+                        frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                         </iframe>
                     </div>
                     """, unsafe_allow_html=True)
