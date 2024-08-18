528a3663878b4dc5886000e61cb963cdb2e72279
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-18 18:01:39 -0300

    Update 02_Backlog.py

--- pages/02_Backlog.py
+++ pages/02_Backlog.py
@@ -7,6 +7,18 @@ st.set_page_config(
     layout="wide"
 )
 
+# We reduced the empty space at the beginning of the streamlit
+reduce_space ="""
+            <style type="text/css">
+            /* Remueve el espacio en el encabezado por defecto de las apps de Streamlit */
+            div[data-testid="stAppViewBlockContainer"]{
+                padding-top:30px;
+            }
+            </style>
+            """
+# We load reduce_space
+st.html(reduce_space)
+
 def centrar_imagen(imagen, ancho):
     # Aplicar estilo CSS para centrar la imagen con Markdown
     st.markdown(
